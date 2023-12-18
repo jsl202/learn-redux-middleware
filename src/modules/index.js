a864a07888345630dd699245db2a4f22c3666082
jsl202 <jsl202@naver.com>
2023-12-18 14:49:39 +0900

    redux-saga

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,12 +1,18 @@
 import { combineReducers } from "redux";
-import counter from "./counter";
+import counter, { counterSaga } from "./counter";
 import sample from "./sample";
 import loading from "./loading";
+import { all } from "redux-saga/effects";
 
 const rootReducer = combineReducers({
-    counter,
-    sample,
-    loading
+  counter,
+  sample,
+  loading,
 });
+
+export function* rootSaga() {
+  // all 함수는 여러 사가를 합쳐 주는 역할을 합니다.
+  yield all([counterSaga()]);
+}
 
 export default rootReducer;
