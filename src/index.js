f49350ed3ebaa2fb439738c4c7e1ee6cd7fc083c
jsl202 <jsl202@naver.com>
2023-12-18 10:55:50 +0900

    redux-logger

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from "./App";
 import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./modules";
 import { Provider } from "react-redux";
-import loggerMiddleware from "./lib/loggerMiddleware";
+import logger from "redux-logger";
 
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
